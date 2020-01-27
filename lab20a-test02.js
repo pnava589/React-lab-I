56e942448d149be5a2640b9e324b1aa91d5b5f66
Pedro <35156354+pnava589@users.noreply.github.com>
2020-01-26 23:59:57 -0700

    commit to master

---
+++ lab20a-test02.js
@@ -0,0 +1,115 @@
+
+
+// In the next lab we will retrieve thhis from an API
+const movieData = [
+    {   id: 2, title: "The Princess Diaries", tagline: "It can take a lifetime to find true love; she's got 30 days!",
+        poster: "/w9Z7A0GHEhIp7etpj0vyKOeU1Wx.jpg"
+    },     
+    {   id: 11, title: "Rocky", tagline: "His whole life was a million-to-one shot",
+        poster: "/i5xiwdSsrecBvO7mIfAJixeEDSg.jpg"
+    },  
+    {   id: 13, title: "American Beauty", tagline: "Look Closer",
+        poster: "/or1MP8BZIAjqWYxPdPX724ydKar.jpg"
+    },  
+    {   id: 14, title: "Be Cool", tagline: "Everyone is looking for the next big hit",
+        poster: "/ekKCH7Zkb5ZTr1f1AczZY23FH.jpg"
+    },  
+    {   id: 15, title: "Minority Report", tagline: "The system is perfect until it comes after you",
+        poster: "/h3lpltSn7Rj1eYTPQO1lYGdw4Bz.jpg"
+    },  
+    {   id: 21, title: "Lost in Space", tagline: "Danger, Will Robinson!",
+        poster: "/kkBDPkUnYwAkWbYY2bnyf5ANObA.jpg"
+    },  
+    {   id: 30, title: "Changing Lanes", tagline: "An ambitious lawyer, a desperate father, they had no reason to meet, until today",
+        poster: "/dCTVH1kl3aIkUmWpMT5STLAwsPs.jpg"
+    },  
+    {   id: 65, title: "Nebraska", tagline: "Life's not about winning or losing. It's about how you get there in the end.",
+        poster: "/zyG3vYWK7SwWT4bvrWeFef0J7fp.jpg"
+    },  
+    {   id: 71, title: "The Jacket", tagline: "Terror has a new name",
+        poster: "/ts5hSSpgHEcPFpdfk6gmLLgx5k4.jpg"
+    },  
+    {   id: 95, title: "Mulholland Drive", tagline: "An actress longing to be a star. A woman searching for herself. Both worlds will collide... on Mulholland Drive",
+        poster: "/oKyY4TFaLjQTgyX8oRde82GinOw.jpg"
+    },   
+    {   id: 102, title: "A Beautiful Mind", tagline: "I need to believe that something extra ordinary is possible",
+        poster: "/4SFqHDZ1NvWdysucWbgnYlobdxC.jpg"
+    },
+    {   id: 128, title: "The Gift", tagline: "The only witness to the crime was not even there",
+        poster: "/mC66QbJk1PbUrVMu3kh14adLpQl.jpg"
+    },
+    {   id: 180, title: "Licence to Kill", tagline: "His bad side is a dangerous place to be",
+        poster: "/27EySoBH3oBuI6xat2QOquB9X8j.jpg"
+    },
+    {   id: 212, title: "Jaws", tagline: "Don't go in the water",
+        poster: "/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg"
+    },                                                     
+    {   id: 229, title: "The Wild Bunch", tagline: "If you only want to spend two hours in a movie theatre and go home and forget it, stay away from THE WILD BUNCH",
+        poster: "/zZhp7p8qvfVrSLKpOFHcKjpEj8f.jpg"
+    }
+];
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------
+*/
+class App extends React.Component {
+
+}
+
+
+  
+/* 
+------------------------------------------------------------------------------------------------------------------
+*/
+const Header = props => {
+
+};
+  
+
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------
+*/
+class FavoritesList extends React.Component {
+
+
+
+}
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------
+*/
+class SingleFavorite extends React.Component {
+
+
+
+}
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------
+*/
+class Movies extends React.Component {
+
+
+
+}
+
+/* 
+------------------------------------------------------------------------------------------------------------------
+*/
+class SingleMovie extends React.Component {
+
+
+
+}
+  
+
+
+/* 
+  ------------------------------------------------------------------------------------------------------------------
+*/
+ReactDOM.render(<App />, document.querySelector('#react-container')); 
